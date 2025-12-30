c9dbe0ca23ef31a895e9e291305c69340f3a712d
Juandre <Juandj@agilebridge.co.za>
2025-12-30 11:03:34 +0200

    Update Playwright configuration to adjust retries and workers for CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   testDir: "./automation/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 2,
+  retries: process.env.CI ? 0 : 0,
+  workers: process.env.CI ? 5 : 2,
   reporter: [["list"], ["allure-playwright"]],
   use: {
     trace: "on-first-retry",
